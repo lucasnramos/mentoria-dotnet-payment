c59d55bc656e0a55c3244b095358119eeca5869b
Lucas Ramos <lucasnramos@gmail.com>
2025-08-12 11:28:15 -0300

    Refactor Worker class to include IConfiguration for dynamic queue configuration

--- PaymentWorker/Worker.cs
+++ PaymentWorker/Worker.cs
@@ -7,20 +7,21 @@ public class Worker : BackgroundService
     private readonly ILogger<Worker> _logger;
     private IDisposable _disposable;
     private readonly Consumer _consumer;
+    private readonly IConfiguration _configuration;
 
-    public Worker(ILogger<Worker> logger, Consumer consumer)
+    public Worker(ILogger<Worker> logger, Consumer consumer, IConfiguration configuration)
     {
         _logger = logger;
         _consumer = consumer;
+        _configuration = configuration;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _disposable = _consumer.Start<OrderMessage>("messagebus.payment.eventhandler", async (message) =>
+        _disposable = _consumer.Start<OrderMessage>(_configuration.GetSection("RabbitMq:QueuePayment").Value, async (message) =>
         {
             _logger.LogInformation("Received OrderMessage: {OrderId}, Customer: {CustomerName}, Email: {CustomerEmail}, Total: {TotalAmount}",
                 message.OrderId, message.CustomerName, message.CustomerEmail, message.TotalAmount);
-            // Here you can add logic to process the order message
         });
     }
 }
